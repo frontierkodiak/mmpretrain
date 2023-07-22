9666598ab5e99bc065c3f4756b490751e79579b6
Caleb Sowers <frontierkodiak@gmail.com>
2023-07-22 03:42:32 -0400

    Trying something

--- configs/_base_/datasets/avesMulti_bs64_swin_224.py
+++ configs/_base_/datasets/avesMulti_bs64_swin_224.py
@@ -1,16 +1,16 @@
 # dataset settings
 dataset_type = 'MultiTaskDataset'
-data_preprocessor = dict(
-    # num_classes=1000, # CLARIFY: What do I need to modify here?
-    # RGB format normalization parameters
-    mean=[123.675, 116.28, 103.53],
-    std=[58.395, 57.12, 57.375],
-    # convert image from BGR to RGB
-    to_rgb=True,
-)
+# data_preprocessor = dict(
+#     # num_classes=1000, # CLARIFY: What do I need to modify here?
+#     # RGB format normalization parameters
+#     mean=[123.675, 116.28, 103.53],
+#     std=[58.395, 57.12, 57.375],
+#     # convert image from BGR to RGB
+#     to_rgb=True,
+# )
 
-bgr_mean = data_preprocessor['mean'][::-1]
-bgr_std = data_preprocessor['std'][::-1]
+# bgr_mean = data_preprocessor['mean'][::-1]
+# bgr_std = data_preprocessor['std'][::-1]
 
 train_pipeline = [
     dict(type='LoadImageFromFile'),
@@ -20,23 +20,23 @@ train_pipeline = [
         backend='pillow',
         interpolation='bicubic'),
     dict(type='RandomFlip', prob=0.5, direction='horizontal'),
-    dict(
-        type='RandAugment',
-        policies='timm_increasing',
-        num_policies=2,
-        total_level=10,
-        magnitude_level=9,
-        magnitude_std=0.5,
-        hparams=dict(
-            pad_val=[round(x) for x in bgr_mean], interpolation='bicubic')),
-    dict(
-        type='RandomErasing',
-        erase_prob=0.25,
-        mode='rand',
-        min_area_ratio=0.02,
-        max_area_ratio=1 / 3,
-        fill_color=bgr_mean,
-        fill_std=bgr_std),
+    # dict(
+    #     type='RandAugment',
+    #     policies='timm_increasing',
+    #     num_policies=2,
+    #     total_level=10,
+    #     magnitude_level=9,
+    #     magnitude_std=0.5,
+    #     hparams=dict(
+    #         pad_val=[round(x) for x in bgr_mean], interpolation='bicubic')),
+    # dict(
+    #     type='RandomErasing',
+    #     erase_prob=0.25,
+    #     mode='rand',
+    #     min_area_ratio=0.02,
+    #     max_area_ratio=1 / 3,
+    #     fill_color=bgr_mean,
+    #     fill_std=bgr_std),
     dict(type='PackInputs'),
 ]
 
