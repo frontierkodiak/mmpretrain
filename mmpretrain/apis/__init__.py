57e523ea6bad142be2f2bd348caaffc24b7a209d
Caleb Sowers <frontierkodiak@gmail.com>
2023-08-01 14:39:54 -0400

    Build multiTask handler for TorchServe

--- mmpretrain/apis/__init__.py
+++ mmpretrain/apis/__init__.py
@@ -8,6 +8,7 @@ from .model import (ModelHub, get_model, inference_model, init_model,
                     list_models)
 from .multimodal_retrieval import (ImageToTextRetrievalInferencer,
                                    TextToImageRetrievalInferencer)
+from .multiTask_image_classification import MultiTaskImageClassificationInferencer
 from .nlvr import NLVRInferencer
 from .visual_grounding import VisualGroundingInferencer
 from .visual_question_answering import VisualQuestionAnsweringInferencer
